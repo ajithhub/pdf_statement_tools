6bcb52af2ab5bfc7de3a66fdb352581c5132f449
Ajith Antony <Ajith.Antony@riverbed.com>
2024-01-31 02:12:42 -0600

    Initial commit with schwab renamer
    OMFG, schwab expects me to do something useful with 50 statements named "Report(...).pdf"

---
+++ README.md
@@ -0,0 +1,37 @@
+# PDF Statement tools
+
+I like to collect and archive my statements and bills.   This is hard to do.
+My office is full of years of hard copies.  If it was easier to gather online
+statements, then I might consider abandoning hard copies.
+
+This repo will maybe be some tools to make collecting and organizing easier.
+
+# Why does digital documentation suck so bad?
+
+Online statements from utilities, banks, and brokers are cumbersome.   They
+charge you fees to keep sending paper statements, but do nothing to make
+collecting online statements convenient.
+
+- They make history search and statement download pages complicated.  Sometimes
+  too javascripty so you can't even just get the links to download.
+- The filenames presented in the browser are not speficied at all, or are too
+  generic to download more than one at a time and keep track of them without
+  renaming individually.
+- The history is only kept for a fixed time.  10yrs is the longest i've seen,
+  but often just 2 yrs or 1 yr. So downloading periodically is a requirement.
+  Further, they so often upgrade or change systems, that the risk of missing
+  data is much higher than just a matter of expiration
+
+
+# FileThis is gone
+
+https://filethis.com/
+
+FileThis used to be a paid service that could login to all your online
+services, locate and download statements, and save them with sensibles names in
+folders in your choice of storage platform, like dropbox.
+
+As of Oct 2023, FileThis went out of business.  It was somewhat unreliable at
+times, but that's understandable considering the hundreds of sites they tried to
+scrape and how fragile scraping is itself.
+
---
+++ pdf_renamer.py
@@ -0,0 +1,120 @@
+from pypdf import PdfReader
+import re
+import logging
+from os import path
+import os
+from glob import glob
+
+"""
+Script to process a directory of similar PDF files, parse the content inside
+the document, and rename the files to organize them
+
+e.g. 40 Charles schwab brokerage statements all named "Report (...).pdf" to be
+renamed by year and quarter
+
+Ideas for features:
+    - Detect the file type and choose a class automatically
+    - better warnings and dry run options for overwriting
+    - config file for pattern matching
+
+"""
+class PDFRenamer():
+    def __init__(self, page=1,pattern=None,output_fmt=None):
+        self.page=page
+        self.pattern=pattern
+        self.output_fmt=output_fmt
+
+    def extract_text(self, file_name):
+        reader = PdfReader(file_name)
+        number_of_pages = len(reader.pages)
+        if number_of_pages < self.page:
+            raise(Exception(f"Not enough pages in f{file_name}"))
+
+        page = reader.pages[0]
+        text = page.extract_text()
+        return text
+
+    def parse_text(self, text):
+        if not self.pattern:
+            raise(Exception("Undefined pattern to match"))
+
+        matches = re.search(self.pattern, text)
+
+        if not matches:
+            raise(Exception("Not matches found"))
+        return matches
+
+    def get_new_filename(self, file_name):
+        text = self.extract_text(file_name)
+        matches = self.parse_text(text)
+        new_file_name = self.transform_matches(matches)
+        return new_file_name
+
+    def transform_matches(self, matches):
+        raise(NotImplementedError())
+
+    def rename(self, file_name):
+        abspath = path.abspath(file_name)
+        new_name = self.get_new_filename(abspath)
+        log = logging.getLogger("rename")
+        dir_name = path.dirname(abspath)
+        new_abs_path = path.join(dir_name, new_name)
+
+        if abspath == new_abs_path:
+            log.info("Name unchanged: %s", abspath)
+            return
+
+        log.info("Renaming %s -> %s", abspath, new_abs_path)
+        os.rename(abspath, new_abs_path)
+
+
+class Schwab401kStatement(PDFRenamer):
+    "Period covered: JANUARY 1,2014 TOMARCH 31,2014 Prepared for: AJITH ANTONY"
+    def __init__(self):
+        pattern = "Period covered: (\w+) (\d+),(\d{4}) TO\w+ \d+,\d{4} Prepared for: AJITH ANTONY"
+
+        return super().__init__( page=1, pattern=pattern)
+
+    def transform_matches(self, matches):
+        start_month = matches.group(1)
+        start_day = matches.group(2)
+        start_year = matches.group(3)
+
+        log.info("Date is %s %s %s", start_month, start_day, start_year)
+
+        quarter_map = {
+                'JANUARY': "Q1",
+                'APRIL': "Q2",
+                'JULY': "Q3",
+                'OCTOBER': "Q4"
+                }
+        if start_month not in quarter_map:
+            raise(Exception(f"Did not understand start month: {start_month}"))
+
+        output_fmt = "{0}-{1}-Schwab-401k.pdf"
+        new_file_name = output_fmt.format(start_year, quarter_map[start_month])
+        return new_file_name
+
+
+
+def get_all_pdfs(directory):
+    def find_ext(dr, ext, ig_case=False):
+        if ig_case:
+            ext =  "".join(["[{}]".format(ch + ch.swapcase()) for ch in ext])
+            return glob(path.join(dr, "*." + ext))
+    return find_ext(directory, "pdf", True)
+
+def get_abs_paths(files):
+    return [path.abspath(f) for f in files]
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger("main")
+
+    file_name = "test.pdf"
+    log.info(f"Processing {file_name}")
+
+    schwab_renamer = Schwab401kStatement()
+
+    for statement in get_abs_paths(get_all_pdfs(".")):
+        schwab_renamer.rename(statement)
